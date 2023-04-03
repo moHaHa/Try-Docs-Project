3c6a22bf8f2bad8656002345ce2d86ca8fe3ac80
moHaHa <diab.4869@gmail.com>
2023-04-03 07:25:55 +0300

    feat : deploy

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+    ],
+  },
+  base: 'https://mohaha.github.io/VitePress-Docs/'
 })
